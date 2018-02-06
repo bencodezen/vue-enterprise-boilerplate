44da903420181d04c19c2a38ebab09d356e655bb
Chris Fritz <chrisvfritz@gmail.com>
2018-02-06 17:35:21 -0500

    init

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  transform: {
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  snapshotSerializers: ["jest-serializer-vue"],
+  mapCoverage: true
+};
