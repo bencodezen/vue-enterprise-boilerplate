3e076aeb17e4e1c187d2b68d258e40cac9104d93
Chris Fritz <chrisvfritz@gmail.com>
2018-02-23 00:08:34 -0500

    rename component to wrapper in base input unit test

--- src/components/_base-input.unit.js
+++ src/components/_base-input.unit.js
@@ -2,19 +2,19 @@ import BaseInput from './_base-input'
 
 describe('@components/base-input', () => {
   it('works with v-model', () => {
-    const component = mount(BaseInput, { propsData: { value: 'aaa' } })
-    const inputEl = component.find('input').element
+    const wrapper = mount(BaseInput, { propsData: { value: 'aaa' } })
+    const inputEl = wrapper.find('input').element
 
     // Has the correct starting value
     expect(inputEl.value).toEqual('aaa')
 
     // Emits an input event with the correct value when edited
     inputEl.value = 'bbb'
-    component.trigger('input')
-    expect(component.emitted().input).toEqual([['bbb']])
+    wrapper.trigger('input')
+    expect(wrapper.emitted().input).toEqual([['bbb']])
 
     // Sets the input to the correct value when props change
-    component.setProps({ value: 'ccc' })
+    wrapper.setProps({ value: 'ccc' })
     expect(inputEl.value).toEqual('ccc')
   })
 })
