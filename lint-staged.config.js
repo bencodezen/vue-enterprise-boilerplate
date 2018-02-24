df5609e76aa901a9161c5fd87fc19fb5eb387b2c
Chris Fritz <chrisvfritz@gmail.com>
2018-02-24 16:23:15 -0500

    improve lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   '*.{js,jsx}': [
     'eslint --fix',
-    'prettier --list-different --write',
+    'prettier --write',
     'git add',
-    'jest --no-cache --findRelatedTests',
+    'jest --bail --findRelatedTests',
   ],
-  '*.json': ['prettier --list-different --write', 'git add'],
+  '*.json': ['prettier --write', 'git add'],
   '*.vue': [
     'eslint --fix',
     'stylelint --fix',
-    'prettier --list-different --write',
+    'prettier --write',
     'git add',
-    'jest --no-cache --findRelatedTests',
+    'jest --no-cache --bail --findRelatedTests',
   ],
-  '*.scss': ['stylelint --fix', 'prettier --list-different --write', 'git add'],
-  '*.md': ['markdownlint', 'prettier --list-different --write', 'git add'],
+  '*.scss': ['stylelint --fix', 'prettier --write', 'git add'],
+  '*.md': ['markdownlint', 'prettier --write', 'git add'],
   '*.{png,jpeg,jpg,gif,svg}': ['imagemin-lint-staged', 'git add'],
 }
