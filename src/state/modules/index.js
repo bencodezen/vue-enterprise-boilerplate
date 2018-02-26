b5a5548cca0896aab2e70d7e77387ed93393f144
Alex Dilley <alex.dilley@gmail.com>
2018-02-26 16:05:11 +0000

    register Vuex modules in subdirectories

--- src/state/modules/index.js
+++ src/state/modules/index.js
@@ -1,16 +1,34 @@
-// Register all Vuex module by a camelCase version of their filename.
+// Register each file as a corresponding Vuex module -- module nesting will
+// mirror [sub-]directory hierarchy. (Modules are namespaced as the camelCase
+// equivalent of their file name.)
 
 import camelCase from 'lodash/camelCase'
 
-const requireModule = require.context('.', false, /\.js$/)
-const modules = {}
+const context = require.context('.', true, /\.js$/)
+const root = { modules: {} }
+const getNamespace = (subtree, path) => {
+  if (path.length === 1) return subtree
 
-requireModule.keys().forEach(fileName => {
-  if (fileName === './index.js' || /\.unit\.js$/.test(fileName)) return
-  modules[camelCase(fileName.replace(/(\.\/|\.js)/g, ''))] = {
+  const namespace = path.shift()
+  subtree.modules[namespace] = {
+    modules: {},
+    ...subtree.modules[namespace],
+  }
+  return getNamespace(subtree.modules[namespace], path)
+};
+
+context.keys().forEach(fileName => {
+  let name = fileName.replace(/^\.\/|\.js$/g, '')
+  if (/(^index|\.unit)$/.test(name)) return
+
+  const path = name.split(/\//)
+  const { modules } = getNamespace(root, path)
+
+  name = path.pop()
+  modules[camelCase(name)] = {
     namespaced: true,
-    ...requireModule(fileName),
+    ...context(fileName),
   }
 })
 
-export default modules
+export default root.modules
