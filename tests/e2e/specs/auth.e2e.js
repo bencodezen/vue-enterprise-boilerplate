e0013a9ac437be97a47f5dcfd5724497bdf7ed2d
Chris Fritz <chrisvfritz@gmail.com>
2018-03-10 18:14:00 -0500

    refactor auth e2e test

--- tests/e2e/specs/auth.e2e.js
+++ tests/e2e/specs/auth.e2e.js
@@ -1,5 +1,5 @@
-describe('Login', () => {
-  it('link exists on the home page when logged out', () => {
+describe('Authentication', () => {
+  it('login link exists on the home page when logged out', () => {
     cy.visit('/')
     cy
       .get('a')
@@ -7,21 +7,19 @@ describe('Login', () => {
       .click()
   })
 
-  it('form shows an error on failure', () => {
+  it('login form shows an error on failure', () => {
     cy.logIn({ username: 'badUsername', password: 'badPassword' })
     cy.contains('error logging in')
   })
 
-  it('form redirects to the home page on success', () => {
+  it('login form redirects to the home page on success', () => {
     cy.logIn()
   })
 
   it('does not show login link when already logged in', () => {
     cy.get('a').should('not.contain', 'Log in')
   })
-})
 
-describe('Logout', () => {
   it('logs the user out when clicking on the "Log out" link', () => {
     cy
       .get('a')
