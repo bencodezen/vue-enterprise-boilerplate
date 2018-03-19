fa63bc4e1cfeaaa5604f3a044e8baaeaeae42fa9
Chris Fritz <chrisvfritz@gmail.com>
2018-03-18 22:45:08 -0400

    prevent console.log on pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,10 @@ module.exports = {
   rules: {
     // Only allow debugger in development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? ['error', { allow: ['warn', 'error'] }]
+        : 'off',
   },
   overrides: [
     {
