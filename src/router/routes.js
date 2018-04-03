b72ef0368c98c8ac950d842141f9a96efd86e8ba
Chris Fritz <chrisvfritz@gmail.com>
2018-04-03 11:02:00 -0400

    add better comments for lazy loading views

--- src/router/routes.js
+++ src/router/routes.js
@@ -90,24 +90,41 @@ export default [
   },
 ]
 
+// Lazy-loads view components, but with better UX. A loading view
+// will be used if the component takes a while to load, falling
+// back to a timeout view in case the page fails to load. You can
+// use this component to lazy-load a route with:
+//
+// component: () => lazyLoadView(import('@views/my-view'))
+//
+// NOTE: Components loaded with this strategy DO NOT have access
+// to in-component guards, such as beforeRouteEnter,
+// beforeRouteUpdate, and beforeRouteLeave. You must either use
+// route-level guards instead or lazy-load the component directly:
+//
+// component: () => import('@views/my-view')
+//
 function lazyLoadView(AsyncView) {
   const AsyncHandler = () => ({
     component: AsyncView,
     // A component to use while the component is loading.
     loading: require('@views/loading').default,
     // A fallback component in case the timeout is exceeded
-    // in loading the component.
+    // when loading the component.
     error: require('@views/timeout').default,
-    // Delay before showing the loading component. Default: 200ms.
-    delay: 200,
-    // The error component will be displayed if a timeout is
-    // provided and exceeded. Default: Infinity.
+    // Delay before showing the loading component.
+    // Default: 200 (milliseconds).
+    delay: 400,
+    // Time before giving up trying to load the component.
+    // Default: Infinity (milliseconds).
     timeout: 10000,
   })
 
   return Promise.resolve({
     functional: true,
     render(h, { data, children }) {
+      // Transparently pass any props or children
+      // to the view component.
       return h(AsyncHandler, data, children)
     },
   })
