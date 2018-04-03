7c0e85ac1a20652ceaedec5f52eeb5102fbc149b
Chris Fritz <chrisvfritz@gmail.com>
2018-04-03 13:27:05 -0400

    refactor usage of jest to comply with 12-factor

--- jest.config.js
+++ jest.config.js
@@ -34,7 +34,8 @@ module.exports = {
   // Set the `testURL` to a provided base URL if one exists, or the mock API base URL
   // Solves: https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios
   testURL:
-    process.env.API_BASE_URL || `http://localhost:${process.env.MOCK_API_PORT}`,
+    process.env.VUE_APP_API_BASE_URL ||
+    `http://localhost:${process.env.MOCK_API_PORT}`,
   globals: {
     'vue-jest': {
       // Disable CSS compilation until it's more stable
