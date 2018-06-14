c0e8bec84b3b0325a4c622059e5955f400a23baf
Chris Fritz <chrisvfritz@gmail.com>
2018-06-14 11:21:54 -0400

    upgrade to use modern mode

--- .babelrc.js
+++ .babelrc.js
@@ -1,35 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@vue/app',
-      {
-        // Disable Babel's polyfills, replaced by the
-        // polyfill.io service in index.html
-        useBuiltIns: false,
-      },
-    ],
-  ],
-  plugins: [
-    // Support async/await
-    // https://babeljs.io/docs/plugins/transform-runtime/
-    [
-      '@babel/transform-runtime',
-      { helpers: true, polyfill: false, regenerator: true },
-    ],
-  ],
-  env: {
-    test: {
-      presets: [
-        [
-          '@vue/app',
-          {
-            // Enable Babel's polyfills for Jest tests
-            useBuiltIns: 'usage',
-            // Use CommonJS modules for Jest tests
-            modules: 'commonjs',
-          },
-        ],
-      ],
-    },
-  },
+  presets: ['@vue/app'],
 }
