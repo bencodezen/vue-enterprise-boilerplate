bb73902086bb41940ceaea751252aaff2586fb93
Chris Fritz <chrisvfritz@gmail.com>
2018-08-26 12:08:30 -0400

    add go-to-definition and peek support for webpack aliases and .vue files

--- aliases.config.js
+++ aliases.config.js
@@ -1,8 +1,6 @@
 const path = require('path')
-
-function resolveSrc(_path) {
-  return path.join(__dirname, _path)
-}
+const fs = require('fs')
+const prettier = require('prettier')
 
 const aliases = {
   '@src': 'src',
@@ -19,10 +17,57 @@ const aliases = {
 module.exports = {
   webpack: {},
   jest: {},
+  jsconfig: {},
 }
 
 for (const alias in aliases) {
-  module.exports.webpack[alias] = resolveSrc(aliases[alias])
-  module.exports.jest['^' + alias + '/(.*)$'] =
-    '<rootDir>/' + aliases[alias] + '/$1'
+  const aliasTo = aliases[alias]
+  module.exports.webpack[alias] = resolveSrc(aliasTo)
+  module.exports.jest['^' + alias + '/(.*)$'] = '<rootDir>/' + aliasTo + '/$1'
+  module.exports.jsconfig[alias + '/*'] = [aliasTo + '/*']
+  module.exports.jsconfig[alias] = aliasTo.includes('/index.')
+    ? [aliasTo]
+    : [
+        aliasTo + '/index.js',
+        aliasTo + '/index.json',
+        aliasTo + '/index.vue',
+        aliasTo + '/index.scss',
+        aliasTo + '/index.css',
+      ]
+}
+
+const jsconfigPath = path.resolve(__dirname, 'jsconfig.json')
+
+fs.writeFile(
+  jsconfigPath,
+  prettier.format(
+    JSON.stringify({
+      baseUrl: '.',
+      checkJs: true,
+      include: ['src/**/*', 'tests/**/*'],
+      exclude: ['node_modules'],
+      compilerOptions: {
+        baseUrl: '.',
+        target: 'es6',
+        module: 'es6',
+        paths: module.exports.jsconfig,
+      },
+    }),
+    {
+      ...require('./.prettierrc'),
+      parser: 'json',
+    }
+  ),
+  error => {
+    if (error) {
+      console.error(
+        'Error while creating jsconfig.json from aliases.config.js.'
+      )
+      throw error
+    }
+  }
+)
+
+function resolveSrc(_path) {
+  return path.resolve(__dirname, _path)
 }
