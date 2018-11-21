cf6b315fee12a457d24ada8f4889c13d912b2c49
Chris Fritz <chrisvfritz@gmail.com>
2018-11-20 20:59:42 -0700

    add a note about strict mode

--- src/state/store.js
+++ src/state/store.js
@@ -7,6 +7,9 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules,
+  // Enable strict mode in development to get a warning
+  // when mutating state outside of a mutation.
+  // https://vuex.vuejs.org/guide/strict.html
   strict: process.env.NODE_ENV !== 'production',
 })
 
