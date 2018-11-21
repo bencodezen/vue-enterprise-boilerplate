e5dfec1a4d9595f8ddfb1a2bbcea0876e0f09162
Chris Fritz <chrisvfritz@gmail.com>
2018-11-20 20:37:43 -0700

    add hot reloading for vuex modules, fixes #108

--- src/state/modules/index.js
+++ src/state/modules/index.js
@@ -4,50 +4,75 @@
 
 import camelCase from 'lodash/camelCase'
 
-// https://webpack.js.org/guides/dependency-management/#require-context
-const requireModule = require.context(
-  // Search for files in the current directory
-  '.',
-  // Search for files in subdirectories
-  true,
-  // Include any .js files that are not unit tests
-  /^((?!\.unit\.).)*\.js$/
-)
+const moduleCache = {}
 const root = { modules: {} }
+;(function updateModules() {
+  // Allow us to dynamically require all Vuex module files.
+  // https://webpack.js.org/guides/dependency-management/#require-context
+  const requireModule = require.context(
+    // Search for files in the current directory.
+    '.',
+    // Search for files in subdirectories.
+    true,
+    // Include any .js files that are not this file or a unit test.
+    /^((?!index|\.unit\.).)*\.js$/
+  )
 
-requireModule.keys().forEach(fileName => {
-  // Skip this file, as it's not a module
-  if (fileName === './index.js') return
+  // For every Vuex module...
+  requireModule.keys().forEach(fileName => {
+    const moduleDefinition = requireModule(fileName)
 
-  // Get the module path as an array
-  const modulePath = fileName
-    // Remove the "./" from the beginning
-    .replace(/^\.\//, '')
-    // Remove the file extension from the end
-    .replace(/\.\w+$/, '')
-    // Split nested modules into an array path
-    .split(/\//)
-    // camelCase all module namespaces and names
-    .map(camelCase)
+    // Skip the module during hot reload if it refers to the
+    // same module definition as the one we have cached.
+    if (moduleCache[fileName] === moduleDefinition) return
 
-  // Get the modules object for the current path
-  const { modules } = getNamespace(root, modulePath)
+    // Update the module cache, for efficient hot reloading.
+    moduleCache[fileName] = moduleDefinition
 
-  // Add the module to our modules object
-  modules[modulePath.pop()] = {
-    // Modules are namespaced by default
-    namespaced: true,
-    ...requireModule(fileName),
+    // Get the module path as an array.
+    const modulePath = fileName
+      // Remove the "./" from the beginning.
+      .replace(/^\.\//, '')
+      // Remove the file extension from the end.
+      .replace(/\.\w+$/, '')
+      // Split nested modules into an array path.
+      .split(/\//)
+      // camelCase all module namespaces and names.
+      .map(camelCase)
+
+    // Get the modules object for the current path.
+    const { modules } = getNamespace(root, modulePath)
+
+    // Add the module to our modules object.
+    modules[modulePath.pop()] = {
+      // Modules are namespaced by default.
+      namespaced: true,
+      ...moduleDefinition,
+    }
+  })
+
+  // If the environment supports hot reloading...
+  if (module.hot) {
+    // Whenever any Vuex module is updated...
+    module.hot.accept(requireModule.id, () => {
+      // Update `root.modules` with the latest definitions.
+      updateModules()
+      // Trigger a hot update in the store.
+      require('../store').default.hotUpdate({ modules: root.modules })
+    })
   }
+})()
 
-  // Recursively get the namespace of the module, even if nested
-  function getNamespace(subtree, path) {
-    if (path.length === 1) return subtree
+// Recursively get the namespace of a Vuex module, even if nested.
+function getNamespace(subtree, path) {
+  if (path.length === 1) return subtree
 
-    const namespace = path.shift()
-    subtree.modules[namespace] = { modules: {}, ...subtree.modules[namespace] }
-    return getNamespace(subtree.modules[namespace], path)
+  const namespace = path.shift()
+  subtree.modules[namespace] = {
+    modules: {},
+    ...subtree.modules[namespace],
   }
-})
+  return getNamespace(subtree.modules[namespace], path)
+}
 
 export default root.modules
