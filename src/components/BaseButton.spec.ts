e9b8e49bca62f5f673ab651fc486d69933998e79
Ben Hong <ben@bencodezen.io>
2024-05-12 23:37:44 -0400

    feat: add base button component

---
+++ src/components/BaseButton.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import BaseButton from './BaseButton.vue'
+
+describe('BaseButton Componenet', () => {
+  it('renders its content', () => {
+    const slotContent = '<strong>Click me!</strong>'
+    const { element } = shallowMount(BaseButton, {
+      slots: {
+        default: slotContent
+      }
+    })
+    expect(element.innerHTML).toContain(slotContent)
+  })
+})
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :class="$style.button">
+    <slot />
+  </button>
+</template>
+
+<style module>
+.button {
+  cursor: pointer;
+  border: none;
+}
+
+.button:disabled {
+  cursor: not-allowed;
+}
+</style>
