b95b8103992ea8024e9ba888741360b86fc294ce
Ben Hong <ben@bencodezen.io>
2024-05-12 21:16:38 -0400

    refactor: flatten tests with components

--- src/components/HelloWorld.spec.ts
+++ src/components/HelloWorld.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import HelloWorld from './HelloWorld.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
