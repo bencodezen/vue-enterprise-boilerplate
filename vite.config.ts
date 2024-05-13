7c980a9c15934043fd44647f2df19a7619a684e3
Ben Hong <ben@bencodezen.io>
2024-05-13 15:12:10 -0400

    config: add auto-imports for vue and vue router

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      // targets to transform
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      // global imports to register
+      imports: ['vue', 'vue-router']
+    }),
     Components({
       dirs: ['src/components', 'src/layouts']
     })
