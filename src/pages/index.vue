99aff1cc6d695dc5a741527a248c6446e7023442
Ben Hong <ben@bencodezen.io>
2024-05-13 14:14:54 -0400

    refactor: remove remaining cli boilerplate

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,7 +14,8 @@ useHead({
 
 <template>
   <AppLayout>
-    <TheWelcome />
+    <h1>Home Page</h1>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
   </AppLayout>
 </template>
 
