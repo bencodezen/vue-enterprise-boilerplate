4cf43813bb2953e0a1fa644f131017631435167f
Ben Hong <ben@bencodezen.io>
2024-05-13 15:43:41 -0400

    refactor: remove test code

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,20 +8,12 @@ useHead({
     }
   ]
 })
-
-const text = ref('')
-const capitalizedText = computed(() => {
-  return text.value.toUpperCase()
-})
 </script>
 
 <template>
   <AppLayout>
     <h1>Home Page</h1>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <BaseInputText v-model="text" />
-    <p>{{ text }} - {{ capitalizedText }}</p>
-    <input type="text" />
   </AppLayout>
 </template>
 
